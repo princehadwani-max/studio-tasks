b92ee85e5da8c15c93f6de6058dcc27c4dbb2c7d
princehadwani-max <princehadwani@gmail.com>
2026-08-13 20:20:27 +0530

    update website

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,12 +1,17 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
+const { Pool } = require('pg');
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-   : {
-   // rejectUnauthorized: false,
-  },
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
 });
+
+module.exports = pool;
 
 pool.on('error', (err) => {
   console.error('Unexpected error on idle Postgres client', err);
